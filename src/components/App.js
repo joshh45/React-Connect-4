6a253a8c893ed62220157b6ad3e5a980876c338f
Josh Ho <hojo1025@gmail.com>
2017-02-16 23:37:48 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+    render(){
+      return (
+        <div>
+          <h1> Hello world </h1>
+        </div>
+      );
+    }
+}
+
+
+
+
+export default App;
