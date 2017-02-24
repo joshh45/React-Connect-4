bbd2ac076ec725fea6ba25e8e13fa52a33c91db3
Josh Ho <hojo1025@gmail.com>
2017-02-24 17:46:11 -0500

    winner is currently displayed

--- src/components/App.js
+++ src/components/App.js
@@ -9,20 +9,28 @@ class App extends Component {
       game: this.props.game,
       boardState: null
     };
+    this.gameOver = this.gameOver.bind(this);
   }
 
   updateBoard() {
     this.setState({ game: this.props.game });
   }
 
+  gameOver() {
+    if(this.props.game.gameOver == false) {
+      return `Player ${this.props.game.currentPlayer} is currently moving`;
+    }
+    else {
+      return `The winner is player ${this.props.game.currentPlayer}`;
+    }
+  }
     render(){
-
 
 
       return (
         <div>
           <h1> Connect 4 </h1>
-          <h4> Player {this.props.game.currentPlayer} is moving </h4>
+          <h4> {this.gameOver()} </h4>
             <Game
              game={this.props.game}
              boardState={this.state.boardState}
